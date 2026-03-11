5752f904c53b7c16401f8e517fc212c305efa1ba
maexftw <maexftw@gmail.com>
2026-03-11 17:25:39 +0100

    chore: initial astro conversion with tinacms setup

---
+++ .tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,48 @@
+// .tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: null,
+  // Get this from tina.io
+  token: null,
+  // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "posts",
+        label: "Posts",
+        path: "src/content/posts",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
---
+++ src/content/posts/hello-world.md
@@ -0,0 +1,5 @@
+---
+title: "Hello from TinaCMS"
+posted: "2026-03-11"
+---
+Welcome to your new editing experience!
